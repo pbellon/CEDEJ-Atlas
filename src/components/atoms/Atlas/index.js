5e8b36626ba94e33dd72091c16415651c37dd270
pbellon <bellon.pierre@gmail.com>
2017-05-16 18:11:14 +0200

    Added usage of react-leaflet in atoms/Atlas

--- src/components/atoms/Atlas/index.js
+++ src/components/atoms/Atlas/index.js
@@ -1,45 +1,79 @@
-import React, { Component, PropTypes } from 'react'
-import styled from 'styled-components'
-import { font, palette } from 'styled-theme'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Map, TileLayer } from 'react-leaflet';
+import { CanvasLayer } from 'utils/leaflet';
+import leafletImage from 'leaflet-image';
+
 import { world } from 'data';
 
 import * as d3 from 'd3';
 
-class Atlas extends Component {
+export default class Atlas extends Component {
   static propTypes = {
     print: PropTypes.bool,
     onRender: PropTypes.func,
     width: PropTypes.number,
-    height: PropTypes.number
-  };
-
-  static defaultPropTypes = {
-    print: false
+    height: PropTypes.number,
   }
 
-  componentDidMount(){
-    const { width, height } = this.props;
-    const proj = d3.geoMercator().scale(110).center([ 55.0, 20.00]);
-    const node =  d3.select(this.refs.canvas).node();
-    const ctx = node.getContext('2d');
-    const path = d3.geoPath(proj).context(ctx);
+  static defaultPropTypes = {
+    print: false,
+  }
+
+  constructor() {
+    super();
+    this.projection = d3.geoMercator().scale(110).center([55.0, 20.00]);
+  }
+
+  layers = {
+    base: {
+      url: 'https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_nolabels/{z}/{x}/{y}.png',
+      maxZoom: 17,
+    },
+    naturalFeatures: {
+      url: 'http://server.arcgisonline.com/ArcGIS/rest/services/Reference/World_Reference_Overlay/MapServer/tile/{z}/{y}/{x}.png',
+      maxZoom: 17,
+    },
+  }
+
+  drawCanvas({ canvas }) {
+    const ctx = canvas.getContext('2d');
+    const path = d3.geoPath(this.projection.proj).context(ctx);
     ctx.strokeStyle = '#bbb';
     ctx.beginPath();
     path(world);
     ctx.stroke();
-    if(this.props.onRender){
-      this.props.onRender(node.toDataURL());
-    }
   }
-  render(){
-    console.log('components.Atlas.render');
-    const { width, height, print } = this.props;
+
+  bindContainer(mapRef) {
+    if (!this.props.onRender) { return; }
+    const map = mapRef.props.leafletElement;
+    console.log('map', mapRef, map);
+    leafletImage(this.map, (err, canvas) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      this.props.onRender(canvas.toDataURL());
+    });
+  }
+
+
+  render() {
+    const position = [0, 0];
+    const { base, naturalFeatures } = this.layers;
+    const { width, height } = this.props;
     return (
-      <canvas
-        width={ width }
-        height={ height }
-        ref='canvas'/>
+      <Map
+        center={position}
+        zoom={13}
+        ref={(ref) => this.bindContainer(ref)}
+        width={width}
+        height={height}
+      >
+        <TileLayer {...base} />
+        <TileLayer {...naturalFeatures} />
+      </Map>
     );
   }
 }
-export default Atlas
