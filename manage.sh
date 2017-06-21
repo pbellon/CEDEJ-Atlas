d369c18615af041ac974811b3ae0cf647ab3c982
pbellon <bellon.pierre@gmail.com>
2017-06-21 17:31:40 +0200

    Added utility files to repo

---
+++ manage.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env zsh
+
+convert(){
+	converted="topo-$2"
+	./node_modules/topojson-server/bin/geo2topo $1=data/$2 > data/$converted
+}
+
+buildData(){
+ convert areas temperature_f.json
+ convert circles circle.json
+ convert patterns aridity.json
+}
+
+
+if [[ "$(type -w $@)" =~ .*function ]];
+then
+	echo Starting "$@"
+	eval $(printf "%q " "$@")
+else
+	echo "Function $@ not found, please check manage.sh file"
+fi
+
