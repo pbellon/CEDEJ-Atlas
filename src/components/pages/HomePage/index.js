1fb758b2de57cfb34d69abd2259255119af188d0
pbellon <bellon.pierre@gmail.com>
2017-09-05 12:19:42 +0200

    Added background to HomePage

--- src/components/pages/HomePage/index.js
+++ src/components/pages/HomePage/index.js
@@ -1,14 +1,43 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
+import styled from 'styled-components'; 
+
 import { MarkdownContent, Button } from 'components';
 import { ContentContainer } from 'containers';
+import Background from './background.png';
+
+const AtlasBackground = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-image: url('${Background}');
+  background-repeat: no-repeat;
+  background-size: cover;
+  z-index: 5;
+  &:before {
+    content: ' ';
+    display: block;
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: 6;
+    background: rgba(255,255,255,0.5);
+  }
+`;
 
 const HomePage = () => {
   return (
-    <ContentContainer>
-      <ReactMarkdown source={ MarkdownContent.Home }/>
-      <Button to='/map'>GO</Button>
-    </ContentContainer>
+    <div>
+      <AtlasBackground />
+      <ContentContainer style={{position: 'relative', zIndex: 10}}>
+        <ReactMarkdown source={ MarkdownContent.Home }/>
+        <Button to='/map'>GO</Button>
+      </ContentContainer>
+    </div>
   );
 };
 
