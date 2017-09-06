fee9a1875a85e911407c11c696c9f742ed047547
pbellon <bellon.pierre@gmail.com>
2017-09-06 10:05:41 +0200

    Restructure LoadindIndicator in 2 components for reusability

---
+++ src/components/atoms/LoadingIcon/index.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import styled, { keyframes } from 'styled-components';
+
+import { palette } from 'styled-theme'; 
+
+
+// took from http://tobiasahlin.com/spinkit/
+const bounce = keyframes`
+0%, 80%, 100% {
+  transform: scale(0);
+} 40% {
+  transform: scale(1.0);
+}
+`;
+
+const Spinner = styled.div`
+  text-align: center;
+  width: 100%;
+`;
+
+const color = (reverse)=>reverse?palette('white', 0):palette('primary', 0)
+
+const Dot = styled.div`
+  width: 1em;
+  height: 1em;
+  background-color: ${({reverse})=>color(reverse)};
+  border-radius: 100%;
+  display: inline-block;
+  animation: ${bounce} 1.4s infinite ease-in-out both;
+  animation-delay: ${({delay=0})=>delay}s;
+`;
+
+const LoadingIcon = ({reverse})=>(
+  <Spinner>
+    <Dot reverse={reverse} delay={-0.32}/>
+    <Dot reverse={reverse} delay={ -0.16}/>
+    <Dot reverse={reverse} />
+  </Spinner>
+);
+
+LoadingIcon.propTypes = {
+  reverse: PropTypes.bool
+};
+
+LoadingIcon.defaultProps = {
+  reverse: false
+};
+
+export default LoadingIcon;
--- src/components/atoms/LoadingIndicator/index.js
+++ src/components/atoms/LoadingIndicator/index.js
@@ -2,15 +2,8 @@ import React from 'react';
 import styled, { keyframes } from 'styled-components';
 
 import { palette } from 'styled-theme'; 
+import { LoadingIcon } from 'components';
 
-// took from http://tobiasahlin.com/spinkit/
-const bounce = keyframes`
-0%, 80%, 100% {
-  transform: scale(0);
-} 40% {
-  transform: scale(1.0);
-}
-`;
 const Holder = styled.div`
   transition: opacity .33 ease-in-out;
   position: absolute;
@@ -26,30 +19,6 @@ const Holder = styled.div`
   pointer-events: ${({isLoading})=>isLoading?'auto':'none'};
 `;
 
-const Spinner = styled.div`
-  text-align: center;
-  width: 100%;
-`;
-
-const Dot = styled.div`
-  width: 1em;
-  height: 1em;
-  background-color: ${palette('primary', 0)};
-  border-radius: 100%;
-  display: inline-block;
-  animation: ${bounce} 1.4s infinite ease-in-out both;
-  animation-delay: ${({delay=0})=>delay}s;
-`;
-
-
-
-const LoadingIcon = ()=>(
-  <Spinner>
-    <Dot delay={-0.32}/>
-    <Dot delay={ -0.16}/>
-    <Dot/>
-  </Spinner>
-);
 
 const LoadingIndicator = ({isLoading=false})=>(
   <Holder isLoading={ isLoading }><LoadingIcon/></Holder>
