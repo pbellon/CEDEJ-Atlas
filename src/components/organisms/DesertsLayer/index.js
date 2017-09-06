46e2f63851c2e72931e79c6d80787002ce86ec9e
pbellon <bellon.pierre@gmail.com>
2017-09-06 16:58:42 +0200

    Improve desert layer behavior

--- src/components/organisms/DesertsLayer/index.js
+++ src/components/organisms/DesertsLayer/index.js
@@ -1,10 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { LayerGroup, Pane, CircleMarker, Tooltip } from 'react-leaflet'; 
-import { Polygon } from 'leaflet'; 
+import { Polygon } from 'leaflet';
+
+import { DesertName } from 'components';
+
 import './DesertsLayer.css';
 
-const DesertsLayer = ({ data }, {map})=>{
+const DesertsLayer = ({ data, minZoom }, {map})=>{
+  const Layers = [];
+  const addToLayers = (ref, scalerank)=>{
+    Layers.push({
+      layer: ref,
+      scalerank: scalerank
+    });
+  };
+  const showLayer = ({ layer: { leafletElement }})=>{
+    leafletElement.openTooltip();
+  };
+
+  const hideLayer = ({ layer: { leafletElement }})=>{
+    leafletElement.closeTooltip();
+  };
+  const checkZoom = ()=>{
+    const zoom = map.getZoom();
+    if(zoom >= minZoom){
+      Layers.filter(({scalerank})=>scalerank>=zoom).forEach(hideLayer);
+      Layers.filter(({scalerank})=>scalerank<zoom).forEach(showLayer);
+    } else {
+      Layers.forEach(hideLayer);
+    }
+  };
+  map.on('zoomend', checkZoom); 
   const Tooltips = data.features.map((feature, key) => {
     const polygon = new Polygon(feature.geometry.coordinates).addTo(map);
     const center = polygon.getBounds().getCenter();
@@ -12,18 +39,19 @@ const DesertsLayer = ({ data }, {map})=>{
     map.removeLayer(polygon);
     return (
       <CircleMarker
+        ref={(ref)=>addToLayers(ref, feature.properties.scalerank)}
         radius={1}
         fill={false}
         stroke={false}
         key={key}
         center={ [center.lng, center.lat ] }>
-        <Tooltip permanent pane={'desert-tooltip'} style={{opacity: 1}}>
-          <span>{ label }</span>
+        <Tooltip pane={'desert-tooltip'} style={{opacity: 1}}>
+          <DesertName desert={ feature }>{ label }</DesertName>
         </Tooltip>
       </CircleMarker>
     );
   });
-
+  checkZoom();
   return (
     <Pane name={'desert-tooltip'} style={{zIndex: 1200}}>
       <LayerGroup>
