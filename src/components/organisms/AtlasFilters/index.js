bc201d9181b62d26fd65201b654a5e3ada329b91
pbellon <bellon.pierre@gmail.com>
2017-09-06 16:03:51 +0200

    Hide filters when sidebar is closed #44

--- src/components/organisms/AtlasFilters/index.js
+++ src/components/organisms/AtlasFilters/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import styled from 'styled-components'; 
 
 import {
@@ -7,13 +8,17 @@ import {
   TemperaturesFilters,
   DryFilters,
 } from 'components';
-
+import { fromSidebar } from 'store/selectors';
 // import { fromLayers } from 'store/selectors';
 
-const Container = styled.div``;
+const Container = styled.div`
+  transition: opacity .5s ease;
+  opacity: ${({visible})=>visible?1:0};
+  pointer-events:${({visible})=>visible?'auto':'none'};
+`;
 
-const AtlasFilters = ()=>(
-  <Container>
+const AtlasFilters = ({visible})=>(
+  <Container visible={visible}>
     <LayerFilterGroup layer={ 'temperatures' } 
       heading={'Aridité et températures'}>
       <AridityFilters/>
@@ -25,4 +30,6 @@ const AtlasFilters = ()=>(
   </Container>
 );
 
-export default AtlasFilters;
+export default connect(state => ({
+  visible: fromSidebar.isOpened(state),
+}))(AtlasFilters);
