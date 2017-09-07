9c68485bff73b19180f972abe06c9fe49359312b
pbellon <bellon.pierre@gmail.com>
2017-09-07 13:07:44 +0200

    Set aridity types on two columns

--- src/components/organisms/AridityFilters/index.js
+++ src/components/organisms/AridityFilters/index.js
@@ -1,15 +1,33 @@
 import React from 'react';
+import styled from 'styled-components';
+
 import { ToggleAridityVisibility } from 'components';
+
+const Cols = styled.div`
+  display: flex;
+  justify-content: space-around;
+`;
+
+
+const Col = styled.div`
+  flex-grow: 1;
+  flex-base: 1;
+`;
+
 
 
 const AridityFilters = ()=>{
   return (
-    <div>
-      <ToggleAridityVisibility label={ 'Hyper Aride' } aridity={ 'hyper'} />
-      <ToggleAridityVisibility label={ 'Aride' } aridity={ 'arid' } />
-      <ToggleAridityVisibility label={ 'Semi Aride' } aridity={ 'semi' }/>
-      <ToggleAridityVisibility label={ 'Sub Humide' } aridity={ 'subHumide' }/>
-    </div>
+    <Cols>
+      <Col>
+        <ToggleAridityVisibility label={ 'Hyper Aride' } aridity={ 'hyper'} />
+        <ToggleAridityVisibility label={ 'Aride' } aridity={ 'arid' } />
+      </Col>
+      <Col>
+        <ToggleAridityVisibility label={ 'Semi Aride' } aridity={ 'semi' }/>
+        <ToggleAridityVisibility label={ 'Sub Humide' } aridity={ 'subHumide' }/>
+      </Col>
+    </Cols>
   );
 }
 export default AridityFilters;
