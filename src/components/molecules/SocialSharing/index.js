4068c2d14dc8e96517d3898ab7abd02731ec37c6
pbellon <bellon.pierre@gmail.com>
2017-09-13 18:24:53 +0200

    Add twitter sharing

--- src/components/molecules/SocialSharing/index.js
+++ src/components/molecules/SocialSharing/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
 import { navbar } from 'utils/styles';
-import { FACEBOOK_APP_ID, FACEBOOK_SDK_VERSION } from 'utils/constants';
+import {
+  FACEBOOK_APP_ID,
+  FACEBOOK_SDK_VERSION,
+  TWEET_INTENT_URL, 
+  TWEET_TEXT,
+  TWEET_HASHTAGS,
+} from 'utils/constants';
+
 import FacebookProvider, { Share } from 'react-facebook';
 import { FacebookIcon, TwitterIcon } from 'components';
+
+const getCurrentHref = ()=>window.location.href;
 
 const Holder = styled.div`
   display: flex;
@@ -12,6 +21,30 @@ const Holder = styled.div`
   align-items: center;
   padding: 0 50px;
 `;
+
+const twitterHref =()=>{
+  const url = getCurrentHref();
+  return `${TWEET_INTENT_URL}?text=${
+    encodeURIComponent(TWEET_TEXT)
+  }&url=${
+    encodeURIComponent(url)
+  }&hashtags=${TWEET_HASHTAGS}`;
+
+}
+const openModal = (href, w, h)=>{
+  let ww = window.innerWidth;
+  let wh = window.innerHeight;
+  let wstyle = `
+  height=${h},width=${w},top=${(wh/2)-h/2},left=${(ww/2)-(w/2)},
+    toolbar=0,location=0
+  `;
+  window.open(href, name, wstyle);
+}
+
+const shareTwitter = ()=>{
+  const href = twitterHref();
+  openModal(href, 600, 320);
+}
 
 const SocialSharing = ()=>(
   <Holder>
@@ -22,7 +55,7 @@ const SocialSharing = ()=>(
         <FacebookIcon width={25} height={25}/>
       </Share>
     </FacebookProvider>
-    <TwitterIcon width={25} height={25}/>
+    <TwitterIcon onClick={shareTwitter} width={25} height={25}/>
   </Holder>
 );
 
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,9 @@
 export const FACEBOOK_APP_ID = '709249142599099';
 
 export const FACEBOOK_SDK_VERSION = 'v2.10';
+
+export const TWEET_INTENT_URL = 'https://twitter.com/intent/tweet';
+
+export const TWEET_TEXT = 'Créer votre carte personnalisé des zones arides sur aridityworldmap.org'; 
+
+export const TWEET_HASHTAGS = ''
