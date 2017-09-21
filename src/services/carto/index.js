36ef75fc6e11307c617342d21feb97e84820e493
pbellon <bellon.pierre@gmail.com>
2017-09-21 14:16:24 +0200

    Add watermark and scale to map preview

--- src/services/carto/index.js
+++ src/services/carto/index.js
@@ -1,7 +1,8 @@
 import renderHtml from './renderHtml';
 import htmlTo from './htmlTo';
 import leafletImage from 'leaflet-image';
-
+import formats from 'utils/formats';
+const { A4px } = formats;
 const carto = {};
 const rotatePreview = ({mapPreview, ...data})=>{
   return new Promise((resolve, reject)=>{
@@ -58,16 +59,87 @@ carto.render = (data) => {
   });
 };
 
+
+const addScaleToCanvas = (canvas, mapRef)=>{
+  const scaleHeight = 17;
+  const scaleWidth = (scale)=>parseInt(scale.style.width.replace('px', ''));
+  const scaleText  = (scale)=>scale.innerText;
+  const ctx = canvas.getContext('2d');
+  let i = 0;
+  const { width: cw, height: ch } = canvas;
+  const scales = mapRef._controlContainer.querySelectorAll('.leaflet-control-scale-line');
+  const kmScale = scales[0];
+  const mileScale = scales[1];
+  // first we draw the km scale.
+  const offy = 100; 
+  const offx = 10;
+  ctx.beginPath();
+  ctx.strokeStyle = '#666';
+  ctx.fillStyle = '#000';
+  ctx.lineWidth = 2;
+  ctx.textAlign = 'end';
+  const kmw = scaleWidth(kmScale);
+  const kmt = scaleText(kmScale);
+  ctx.moveTo(cw - offx, ch - offy - scaleHeight);
+  ctx.lineTo(cw - offx, ch - offy);
+  ctx.lineTo(cw - offx - kmw, ch - offy);
+  ctx.lineTo(cw - offx - kmw, ch - offy - scaleHeight);
+  ctx.stroke();
+  ctx.fillText(kmt, cw - offx - 4, ch - offy - 4, kmw); 
+  // then the mile scale
+  const mw = scaleWidth(mileScale);
+  const mt = scaleText(mileScale);
+  ctx.moveTo(cw - offx, ch - (offy - scaleHeight));
+  ctx.lineTo(cw - offx, ch - offy);
+  ctx.lineTo(cw - offx - mw, ch - offy);
+  ctx.lineTo(cw - offx - mw, ch - (offy - scaleHeight));
+  ctx.stroke();
+  ctx.fillText(mt, cw - offx - 4, ch - (offy - 14), mw); 
+}
+
+const addWatermarkToCanvas = (canvas, mapRef)=>{
+  return new Promise((resolve, reject)=>{
+    const ctx = canvas.getContext('2d');
+    const { width: cw, height: ch } = canvas;
+    const watermark = mapRef._controlContainer.querySelector('.leaflet-watermark img');
+    const { width: iw, height: ih } = watermark;
+    const img = new Image();
+    const offx = 10;
+    const offy = 10;
+    img.src = watermark.src;
+    img.onload = ()=>{
+      ctx.drawImage(img, cw - offx - iw, ch - offy - ih, iw, ih);
+      resolve(canvas);
+    }
+  });
+};
+
 carto.preview = (mapRef)=>{
-  return new Promise((resolve, reject) => {
-    leafletImage(mapRef, (err, canvas)=>{
-      if(err){
-        reject(err);
-      } else {
-        resolve(canvas.toDataURL());
+  const addMapOverlay = ({canvas, mapRef})=>{
+    return new Promise((resolve, reject)=>{
+      try {
+        addScaleToCanvas(canvas, mapRef);
+        addWatermarkToCanvas(canvas, mapRef).then((canvas)=>{
+          resolve(canvas.toDataURL('image/jpeg', 1));
+        }).catch(reject);
+      } catch (e) {
+        reject(e);
       }
     });
-  });
+  }
+  const mapToImage = (mapRef)=>{
+    return new Promise((resolve, reject) => {
+      leafletImage(mapRef, (err, canvas)=>{
+        if(err){
+          reject(err);
+        } else {
+          resolve({canvas, mapRef});
+        }
+      }, {x: A4px[0], y: A4px[1]});
+    });
+  };
+  
+  return mapToImage(mapRef).then(addMapOverlay);
 }
 
 carto.download = ({ format, url }) => {
--- src/utils/formats.js
+++ src/utils/formats.js
@@ -1,5 +1,9 @@
 const FORMATS = {
   PDF: 'pdf',
   PNG: 'png',
+  A4: [297,210],
 }
+
+FORMATS.A4px = FORMATS.A4.map(mm => mm*3.779528);
+
 export default FORMATS;
