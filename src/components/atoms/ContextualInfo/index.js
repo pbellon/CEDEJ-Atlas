343556bf5129bc8b4daa1362781e33ef3d819fbd
pbellon <bellon.pierre@gmail.com>
2017-11-09 16:33:22 +0100

    Implemented i18n for ContextualInfo component #88

--- src/components/atoms/ContextualInfo/index.js
+++ src/components/atoms/ContextualInfo/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
+import { translate } from 'react-i18next';
+
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
 import * as circlesUtils from 'utils/circles';
 import * as temperaturesUtils from 'utils/temperatures';
 import * as aridityUtils from 'utils/aridity';
-
-const Holder = styled.div`
-`;
 
 const InfoList = styled.ul`
   list-style: none;
@@ -29,37 +28,31 @@ InfoRow.propTypes = {
 };
 
 const TemperaturesInfo = ({
+  t,
   temperatures: {
     properties: { Temperatur },
   },
 }) => {
   const summerRange = temperaturesUtils.getSummerRange(Temperatur);
+  const [ s0, s1 ] = summerRange.length > 1 ? summerRange : [ summerRange[0], null ];
   const winterRange = temperaturesUtils.getWinterRange(Temperatur);
+  const [ w0, w1 ] = winterRange.length > 1 ? winterRange : [ winterRange[0], null ];
+  
   return (
     <Info>
       <InfoRow
-        title={'Températures en été'}
+        title={t('popup.summerTemperatures')}
         info={
           <span>
-            {(summerRange.length === 1) && (
-              <span>Plus de { summerRange[0] } degrés</span>
-            )}
-            { (summerRange.length > 1) && (
-              <span>Entre { summerRange[0] } et { summerRange[1] } degrés</span>
-            )}
+            { t('popup.summerRange', { count:summerRange.length, s0, s1 }) }
           </span>
         }
       />
       <InfoRow
-        title={'Températures en hiver'}
+        title={t('popup.winterTemperatures')}
         info={
           <span>
-            {(winterRange.length === 1) && (
-              <span>Moins de { winterRange[0] } degrés</span>
-            )}
-            { (winterRange.length > 1) && (
-              <span>Entre { winterRange[0] } et { winterRange[1] } degrés</span>
-            )}
+            { t('popup.winterRange', { count:winterRange.length, w0, w1 }) }
           </span>
         }
       />
@@ -72,12 +65,13 @@ TemperaturesInfo.propTypes = {
 };
 
 const AridityInfo = ({
+  t,
   aridity: {
     properties: { d_TYPE },
   },
 }) => (
   <Info>
-    <InfoRow title={'Type d\'aridité'} info={aridityUtils.getName(d_TYPE)} />
+    <InfoRow title={t('popup.aridityType')} info={aridityUtils.getName(d_TYPE)} />
   </Info>
 );
 
@@ -86,6 +80,7 @@ AridityInfo.propTypes = {
 };
 
 const CirclesInfo = ({
+  t,
   circles: {
     properties: {
       size_,
@@ -93,27 +88,23 @@ const CirclesInfo = ({
     },
   },
 }) => {
-  const droughtMonths = circlesUtils.getDroughtMonths(size_);
+  const dRange = circlesUtils.getDroughtMonths(size_);
+  const [ d0, d1 ] = dRange.length > 1 ? dRange : [dRange[0]];
   return (
     <Info>
       <InfoRow
-        title={'Régime de précipitation'}
+        title={t('popup.precipitationRegimeLabel')}
         info={
           <span>{ circlesUtils.droughtRegimeSingle(colours) }</span>
         }
       />
       <InfoRow
-        title={'Nombre de mois de sécheresse'}
+        title={t('popup.droughtMonthsLabel')}
         info={
           <span>
-            { size_ === '01' && (<span>Moins de &nbsp;</span>)}
-            { droughtMonths.length > 1 && (
-              <span>De { droughtMonths[0] } à { droughtMonths[1] }&nbsp;</span>
-            )}
-            { droughtMonths.length === 1 && (
-              <span>{ droughtMonths[0] }&nbsp;</span>
-            )}
-            mois
+            {
+                t(`popup.droughtMonths_${size_ === '01' ? 0 : dRange.length}`, { d0,d1 })
+            }
           </span>
         }
       />
@@ -125,14 +116,18 @@ CirclesInfo.propTypes = {
   circles: PropTypes.object,
 };
 
-const ContextualInfo = ({ data: { temperatures, aridity, circles } }) => {
+const Holder = styled.div`
+  min-width: 280px;
+`;
+
+const ContextualInfo = ({ t, data: { temperatures, aridity, circles } }) => {
   if (!temperatures && !aridity && !circles) { return null; }
   return (
     <Holder>
       <InfoList>
-        { temperatures && (<TemperaturesInfo temperatures={temperatures} />) }
-        { aridity && (<AridityInfo aridity={aridity} />) }
-        { circles && (<CirclesInfo circles={circles} />) }
+        { temperatures && (<TemperaturesInfo t={t} temperatures={temperatures} />) }
+        { aridity && (<AridityInfo t={t} aridity={aridity} />) }
+        { circles && (<CirclesInfo t={t} circles={circles} />) }
       </InfoList>
     </Holder>
   );
@@ -142,4 +137,4 @@ ContextualInfo.propTypes = {
   data: PropTypes.object,
 };
 
-export default ContextualInfo;
+export default translate('atlas')(ContextualInfo);
