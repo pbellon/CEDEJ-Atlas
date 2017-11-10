fbad651ed7e4b83ea897aedaf728246b98bd361f
pbellon <bellon.pierre@gmail.com>
2017-11-10 15:40:16 +0100

    Add loging for detect_locales

--- src/utils/locales.js
+++ src/utils/locales.js
@@ -1,6 +1,8 @@
 import i18n from '../i18n';
+
 export const detect_locales = (match) => {
   let locale = match.params.locale;
+  console.log('detect_locales',  locale, match);
   if (locale && i18n.languages.indexOf(locale) > -1) {
     i18n.changeLanguage(locale);
   }
