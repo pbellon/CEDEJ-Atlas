a0ab3938ed75f707143fc89ef8e5079251dc3488
pbellon <bellon.pierre@gmail.com>
2017-11-10 15:56:21 +0100

    Change behavior of detect_locales

--- src/utils/locales.js
+++ src/utils/locales.js
@@ -3,7 +3,13 @@ import i18n from '../i18n';
 export const detect_locales = (match) => {
   let locale = match.params.locale;
   if (locale && i18n.options.whitelist.indexOf(locale) > -1) {
-    i18n.changeLanguage(locale);
+    if(i18n.language !== locale){
+      i18n.changeLanguage(locale);
+    }
+  } else {
+    if(i18n.language !== 'fr'){
+      i18n.changeLanguage('fr');
+    }
   }
 }
 
